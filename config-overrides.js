f13d573b4bf0b8bfbe0010353275ab91c7f8bbca
Birk15 <birk.dinkelacker2009@gmail.com>
2024-12-13 08:32:16 +0100

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -9,3 +9,9 @@ module.exports = function override(config, env) {
 
   return config;
 };
+module.exports = {
+  // Handle 404 redirects for GitHub Pages
+  devServer: {
+    historyApiFallback: true,
+  },
+};
